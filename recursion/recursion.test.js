2bcf4ae01a6b259aba07c63877cd8504981067e8
Ahmadhyari <97673048+Ahmadhyari@users.noreply.github.com>
2022-02-04 15:56:07 +0200

    Update recursion.test.js

--- recursion/recursion.test.js
+++ recursion/recursion.test.js
@@ -69,3 +69,58 @@ function fibonacci(n) {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
+
+/* Optional 
+The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+Example: 
+Input: n = 3, k = 3
+Output: ["123", "132", "213", "231", "312", "321"] */
+
+function permutations(n, k) {
+    let arr = [];
+    // Write you logic here. 
+    return arr
+};
+
+
+describe("Test division", () => {
+    test("Return the division result", () => {
+        expect(division(10, 2)).toStrictEqual(5);
+        expect(division(10, 0)).toStrictEqual(0);
+        expect(division(0, 10)).toStrictEqual(0);
+
+    })
+});
+
+describe("Test pow", () => {
+    test("It should work as Math.pow(x,n)", () => {
+        expect(pow(10, 2)).toStrictEqual(100);
+        expect(pow(10, 0)).toStrictEqual(1);
+        expect(pow(0, 0)).toStrictEqual(1);
+    })
+});
+
+describe("Test fibonacci", () => {
+    test("It should implement fibonacci series logic", () => {
+        expect(fibonacci(0)).toStrictEqual(0);
+        expect(fibonacci(1)).toStrictEqual(1);
+        expect(fibonacci(2)).toStrictEqual(1);
+        expect(fibonacci(3)).toStrictEqual(2);
+        expect(fibonacci(4)).toStrictEqual(3);
+    })
+});
+
+describe("Test permutations", () => {
+    test("It should return a list of possible combinations", () => {
+        expect(permutations(3, 3)).toStrictEqual(["123", "132", "213", "231", "312", "321"]);
+        expect(permutations(3, 0)).toStrictEqual([]);
+    })
+});
